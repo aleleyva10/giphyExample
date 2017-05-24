435e6df4139286255bf84c2b4e146400c270d662
devjanaprime <dev@primeacademy.io>
2017-05-24 09:43:43 -0500

    updated from omdb

---
+++ scripts/apiExample.js
@@ -0,0 +1,17 @@
+console.log( 'api example script sourced' );
+
+$( document ).on( 'click', '#searchNow', function(){
+  // assemble search URL
+  var searchURL = 'http://api.giphy.com/v1/gifs/search?q=batman&api_key=dc6zaTOxFJmzC';
+  // make ajax call to OMDB to retrieve JSON
+  $.ajax({
+    url: searchURL,
+    dataType: 'JSON',
+    success: function( data ){
+      // successfully hit API
+      console.log( 'successful API hit:', data );
+      // show results
+      // '.Search' is the array of movie objects returned by OMDB
+    } // end success
+  }); //end ajax
+}); // end click on search button
